874fee75c2194b643b077532adb0d0cc572125e0
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-01-19 22:02:08 -0500

    Initial project commit

---
+++ Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
@@ -0,0 +1,13 @@
+package com.example.uofthacks.boop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainApp extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_app);
+  }
+}
