4e482af0888118b6b1e2c3c5b55c654d22ff0cdf
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-01-20 02:17:27 -0500

    Added click()

--- Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
+++ Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
@@ -3,15 +3,20 @@ package com.example.uofthacks.boop;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainApp extends AppCompatActivity {
   private static final String TAG = "NFC";
   private NfcAdapter nfcAdapter;
-  private PendingIntent nfcPendingIntent;
 
 
   @Override
@@ -21,7 +26,7 @@ public class MainApp extends AppCompatActivity {
 
     // initialize NFC
     nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-    nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    //nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
   }
 
   @Override
@@ -29,20 +34,44 @@ public class MainApp extends AppCompatActivity {
     super.onResume();
     Log.d(TAG, "enableForegroundMode");
 
+    /*
+
     // foreground mode gives the current active application priority for reading scanned tags
     IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED); // filter for tags
     IntentFilter[] writeTagFilters = new IntentFilter[] {tagDetected};
     nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, writeTagFilters, null);
+    */
+    super.onResume();
+    Intent intent = getIntent();
+    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+      Parcelable[] rawMessages = intent.getParcelableArrayExtra(
+          NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+      NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
+      Toast.makeText(this, message.getRecords()[0].getPayload().toString(), Toast.LENGTH_SHORT).show();
+    }
   }
 
-  @Override
-  protected void onNewIntent(Intent intent) {
-    super.onNewIntent(intent);
+  public void onButtonClick(View v){
+    sendFile();
+  }
 
-    Log.d(TAG, "onNewIntent");
+  private void sendFile(){
+    if (!nfcAdapter.isEnabled()){
+      startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
+      Toast.makeText(this, "Please enable NFC", Toast.LENGTH_LONG).show();
+    }
+    else if(!nfcAdapter.isNdefPushEnabled()) {
+      Toast.makeText(this, "Please enable Android Beam.", Toast.LENGTH_SHORT).show();
+    }
+    else{
+      // SEND FILES HERE!
+      Toast.makeText(this, "SENDING FILE!!", Toast.LENGTH_SHORT).show();
 
-    // check for NFC related actions
-    if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+      String message = "SAMPLE DEMO";
+      NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
+      NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+      nfcAdapter.setNdefPushMessage(ndefMessage, this);
     }
   }
 }
