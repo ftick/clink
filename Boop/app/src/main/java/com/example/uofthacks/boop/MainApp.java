9f43a24a8ba7fbfeb9be6b763c948fcf2a6ab6a0
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-01-20 08:29:49 -0500

    Added feature

--- Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
+++ Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
@@ -1,8 +1,6 @@
 package com.example.uofthacks.boop;
 
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -14,35 +12,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback{
+public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback{
   private static final String TAG = "NFC";
-  private NfcAdapter nfcAdapter;
-  private PendingIntent nfcPendingIntent;
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_app);
-
-    // initialize NFC
-    nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-    nfcAdapter.setNdefPushMessageCallback(this, this);
   }
 
   @Override
   protected void onResume() {
     super.onResume();
+    receiveMoney();
+  }
+
+  public void onButtonClick(View v){
+    sendFile();
+  }
+
+  private void receiveMoney(){
     Log.d(TAG, "enableForegroundMode");
 
-    /*
-
-    // foreground mode gives the current active application priority for reading scanned tags
-    IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED); // filter for tags
-    IntentFilter[] writeTagFilters = new IntentFilter[] {tagDetected};
-    nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, writeTagFilters, null);
-    */
-    super.onResume();
     Intent intent = getIntent();
     if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
       Parcelable[] rawMessages = intent.getParcelableArrayExtra(
@@ -53,11 +44,10 @@ public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefM
     }
   }
 
-  public void onButtonClick(View v){
-    sendFile();
-  }
-
   private void sendFile(){
+    // initialize NFC
+    NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
     if (!nfcAdapter.isEnabled()){
       startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
       Toast.makeText(this, "Please enable NFC", Toast.LENGTH_LONG).show();
@@ -68,14 +58,32 @@ public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefM
     else{
       // SEND FILES HERE!
       Toast.makeText(this, "SENDING FILE!!", Toast.LENGTH_SHORT).show();
+      nfcAdapter.setNdefPushMessageCallback(this, this);
+      nfcAdapter.setOnNdefPushCompleteCallback(this, this);
     }
   }
 
+  /**
+   * It is called when the phones clinked.
+   * @param event
+   * @return
+   */
   @Override
   public NdefMessage createNdefMessage(NfcEvent event) {
     String message = "SAMPLE DEMO";
     NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
     NdefMessage ndefMessage = new NdefMessage(ndefRecord);
     return ndefMessage;
+  }
+
+  /**
+   * Is called when the sending of data to the other phone was successful.
+   * @param event
+   */
+  @Override
+  public void onNdefPushComplete(NfcEvent event) {
+    event.nfcAdapter.setNdefPushMessage(null, this);
+    event.nfcAdapter.setNdefPushMessageCallback(null, this);
+    event.nfcAdapter.setOnNdefPushCompleteCallback(null, this);
   }
 }
