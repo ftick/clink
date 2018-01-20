3de4eece2234cee5462ae5c73d404de0f438c2fc
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-01-20 10:09:02 -0500

    Moved NFC to new class

--- Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
+++ Boop/app/src/main/java/com/example/uofthacks/boop/MainApp.java
@@ -13,8 +13,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback{
-  private static final String TAG = "NFC";
+public class MainApp extends AppCompatActivity{
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,71 +24,8 @@ public class MainApp extends AppCompatActivity implements NfcAdapter.CreateNdefM
   @Override
   protected void onResume() {
     super.onResume();
-    receiveMoney();
   }
 
   public void onButtonClick(View v){
-    sendFile();
-  }
-
-  private void receiveMoney(){
-    Log.d(TAG, "enableForegroundMode");
-
-    Intent intent = getIntent();
-    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-      Parcelable[] rawMessages = intent.getParcelableArrayExtra(
-          NfcAdapter.EXTRA_NDEF_MESSAGES);
-
-      NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
-      Toast.makeText(this, message.getRecords()[0].getPayload().toString(), Toast.LENGTH_SHORT).show();
-    }
-  }
-
-  private void sendFile(){
-    // initialize NFC
-    NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-    if (!nfcAdapter.isEnabled()){
-      startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
-      Toast.makeText(this, "Please enable NFC", Toast.LENGTH_LONG).show();
-    }
-    else if(!nfcAdapter.isNdefPushEnabled()) {
-      Toast.makeText(this, "Please enable Android Beam.", Toast.LENGTH_SHORT).show();
-    }
-    else{
-      // SEND FILES HERE!
-      Toast.makeText(this, "SENDING FILE!!", Toast.LENGTH_SHORT).show();
-      TextView waitingForReceipientText = (TextView) findViewById(R.id.waitingForRecipientLabel);
-      waitingForReceipientText.setVisibility(View.VISIBLE);
-      nfcAdapter.setNdefPushMessageCallback(this, this);
-      nfcAdapter.setOnNdefPushCompleteCallback(this, this);
-    }
-  }
-
-  /**
-   * It is called when the phones clinked.
-   * @param event
-   * @return
-   */
-  @Override
-  public NdefMessage createNdefMessage(NfcEvent event) {
-    String message = "SAMPLE DEMO";
-    NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
-    NdefMessage ndefMessage = new NdefMessage(ndefRecord);
-    return ndefMessage;
-  }
-
-  /**
-   * Is called when the sending of data to the other phone was successful.
-   * @param event
-   */
-  @Override
-  public void onNdefPushComplete(NfcEvent event) {
-    Toast.makeText(this, "Money transfer sent", Toast.LENGTH_LONG).show();
-    TextView waitingForReceipientText = (TextView) findViewById(R.id.waitingForRecipientLabel);
-    waitingForReceipientText.setVisibility(View.INVISIBLE);
-    event.nfcAdapter.setNdefPushMessage(null, this);
-    event.nfcAdapter.setNdefPushMessageCallback(null, this);
-    event.nfcAdapter.setOnNdefPushCompleteCallback(null, this);
   }
 }
--- Boop/app/src/main/java/com/example/uofthacks/boop/NFCTransfer.java
+++ Boop/app/src/main/java/com/example/uofthacks/boop/NFCTransfer.java
@@ -1,11 +1,80 @@
 package com.example.uofthacks.boop;
 
-public class NFCTransfer {
-  public boolean transferMoney(MoneyTransfer transfer){
-   return false;
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NFCTransfer implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
+
+  private static final String TAG = "NFC";
+  private Activity activity;
+
+  public NFCTransfer(Activity activity){
+    this.activity = activity;
   }
 
-  public boolean recieveMoney(MoneyTransfer transfer){
+  public boolean transferMoney(MoneyTransfer transfer){
+    // initialize NFC
+    NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(activity);
+
+    if (!nfcAdapter.isEnabled()){
+      activity.startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
+      Toast.makeText(activity, "Please enable NFC", Toast.LENGTH_LONG).show();
+    }
+    else if(!nfcAdapter.isNdefPushEnabled()) {
+      Toast.makeText(activity, "Please enable Android Beam.", Toast.LENGTH_SHORT).show();
+    }
+    else{
+      // SEND FILES HERE!
+      Toast.makeText(activity, "SENDING FILE!!", Toast.LENGTH_SHORT).show();
+      TextView waitingForReceipientText = (TextView) activity.findViewById(R.id.waitingForRecipientLabel);
+      waitingForReceipientText.setVisibility(View.VISIBLE);
+      //nfcAdapter.setNdefPushMessageCallback(this, this);
+      //nfcAdapter.setOnNdefPushCompleteCallback(this, this);
+      return true;
+    }
     return false;
+  }
+
+  public boolean receiveMoney(MoneyTransfer transfer){
+    Log.d(TAG, "enableForegroundMode");
+
+    Intent intent = activity.getIntent();
+    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+      Parcelable[] rawMessages = intent.getParcelableArrayExtra(
+          NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+      NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
+      String text = new String(message.getRecords()[0].getPayload());
+      Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public NdefMessage createNdefMessage(NfcEvent event) {
+    String message = "SAMPLE DEMO";
+    NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
+    NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+    return ndefMessage;
+  }
+
+  @Override
+  public void onNdefPushComplete(NfcEvent event) {
+    Toast.makeText(activity, "Money transfer sent", Toast.LENGTH_LONG).show();
+    TextView waitingForReceipientText = (TextView) activity.findViewById(R.id.waitingForRecipientLabel);
+    waitingForReceipientText.setVisibility(View.INVISIBLE);
+    event.nfcAdapter.setNdefPushMessage(null, activity);
+    event.nfcAdapter.setNdefPushMessageCallback(null, activity);
+    event.nfcAdapter.setOnNdefPushCompleteCallback(null, activity);
   }
 }
